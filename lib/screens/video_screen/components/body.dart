b6619f9e26e83085dfe0a4d847432123d0f4d9ed
Chirag Pandey <pndy.chrg2003@gmail.com>
2022-02-09 13:59:12 +0530

    Created Video Screen

--- lib/screens/video_screen/components/body.dart
+++ lib/screens/video_screen/components/body.dart
@@ -1,13 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:yt_clone/data.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
-class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
+class Body extends StatefulWidget {
+  const Body({
+    Key? key,
+    required this.video,
+  }) : super(key: key);
 
+  final Video video;
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  bool pressed = false;
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          Image.network(
+            widget.video.thumbnailUrl,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: 220,
+          ),
+          //Title
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Expanded(
+                child: Text(
+              widget.video.title,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            )),
+          ),
+          //views row
+          Padding(
+            padding: const EdgeInsets.only(top: 1.0, left: 10),
+            child: Opacity(
+              opacity: 0.5,
+              child: Row(
+                children: <Widget>[
+                  Text(widget.video.viewCount + " views "),
+                  SizedBox(
+                    width: 5,
+                    child: Text("|"),
+                  ),
+                  Text(" ${timeago.format(widget.video.timestamp)}"),
+                ],
+              ),
+            ),
+          ),
+          //Icon Row
+          SizedBox(
+            height: 10,
+          ),
+          //User Panel
+          Container(
+            padding: EdgeInsets.only(top: 5, bottom: 5),
+            decoration: BoxDecoration(
+                border: Border.symmetric(
+              vertical: BorderSide.none,
+              horizontal: BorderSide(
+                width: 1,
+                color: Colors.grey.shade300,
+              ),
+            )),
+            child: Row(
+              children: <Widget>[
+                //id Logo
+                Expanded(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 25,
+                        backgroundImage:
+                            NetworkImage(widget.video.author.profileImageUrl),
+                      ),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      //Profile Name and Subscribe Button
+                      Container(
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              widget.video.author.username,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  if (pressed == true) {
+                                    pressed = false;
+                                  } else {
+                                    pressed = true;
+                                  }
+                                });
+                              },
+                              child: Text(
+                                pressed == false ? "Subscribe" : "Subscribed",
+                                style: TextStyle(
+                                  color: pressed == false
+                                      ? Colors.blue
+                                      : Colors.grey,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      //Subsribers and Join Button
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Opacity(
+                              opacity: 0.5,
+                              child: Text(widget.video.author.subscribers +
+                                  " subscribers"),
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Text(
+                              "JOIN",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.only(right: 10),
+                    child: Icon(Icons.notifications)),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/video_screen/video_screen.dart
+++ lib/screens/video_screen/video_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:yt_clone/data.dart';
 import 'package:yt_clone/screens/video_screen/components/body.dart';
 
 class VideoScreen extends StatelessWidget {
   final Video video;
-  const VideoScreen({Key? key, required this.video}) : super(key: key);
-
+  VideoScreen({Key? key, required this.video}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(child: Image.network(video.thumbnailUrl)),
+    return SafeArea(
+      child: Scaffold(
+        body: Body(video: video),
+      ),
     );
   }
 }
